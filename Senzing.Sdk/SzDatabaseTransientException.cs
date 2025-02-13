0771b9d2be4abf5f138024253d5058fd9ace0a76
Barry M. Caceres <barrycaceres@icloud.com>
2025-02-13 15:35:19 -0800

    Added SzDatabaseTransientException and mappings to it (#62)

---
+++ Senzing.Sdk/SzDatabaseTransientException.cs
@@ -0,0 +1,95 @@
+using System;
+
+namespace Senzing.Sdk
+{
+    /// <summary>
+    /// Extends <see cref="SzRetryableException"/> to define an exceptional
+    /// condition where an operation failed because a database condition
+    /// that is transient and would like be resolved on a repeated attempt.
+    /// Retrying the operation may result in it completing successfully.
+    /// </summary>
+    public class SzDatabaseTransientException : SzRetryableException
+    {
+        /// <summary>Default constructor.</summary>
+        public SzDatabaseTransientException()
+            : base()
+        {
+            // do nothing
+        }
+
+        /// <summary>
+        /// Constructs with a message explaing the reason for the exception.
+        /// </summary>
+        /// 
+        /// <param name="message">
+        /// The message explaining the reason for the exception.
+        /// </param>
+        public SzDatabaseTransientException(string message)
+            : base(message)
+        {
+            // do nothing
+        }
+
+        /// <summary>
+        /// Constructs with a message explaing the reason for the exception.
+        /// </summary>
+        /// 
+        /// <param name="errorCode">The underlying Senzing error code.</param>
+        ///
+        /// <param name="message">
+        /// The message explaining the reason for the exception.
+        /// </param>
+        public SzDatabaseTransientException(long? errorCode, string message)
+            : base(errorCode, message)
+        {
+            // do nothing
+        }
+
+        /// <summary>
+        /// Constructs with the {@link Throwable} that is the underlying cause
+        /// for the exception.
+        /// </summary>
+        /// 
+        /// <param name="cause">The underlying cause for the exception.</param>
+        public SzDatabaseTransientException(Exception cause)
+            : base(null, cause)
+        {
+            // do nothing
+        }
+
+        /// <summary>
+        /// Constructs with a message explaing the reason for the exception and
+        /// the <c>Exception</c> that is the underlying cause for the exception.
+        /// </summary>
+        /// 
+        /// <param name="message">
+        /// The message explaining the reason for the exception.
+        /// </param>
+        ///
+        /// <param name="cause">The underlying cause for the exception.</param>
+        public SzDatabaseTransientException(string message, Exception cause)
+            : base(message, cause)
+        {
+            // do nothing
+        }
+
+        /// <summary>
+        /// Constructs with the Senzing error code, the message explaing the reason
+        /// for the exception and the <c>Exception</c> that is the underlying cause
+        /// for the exception.
+        /// </summary>
+        ///
+        /// <param name="errorCode">The underlying Senzing error code.</param>
+        ///
+        /// <param name="message">
+        /// The message explaining the reason for the exception.
+        /// </param>
+        ///
+        /// <param name="cause">The underlying cause for the exception.</param>
+        public SzDatabaseTransientException(long? errorCode, string message, Exception cause)
+            : base(errorCode, message, cause)
+        {
+            // do nothing
+        }
+    }
+}
